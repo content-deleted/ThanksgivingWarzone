1df3e65a3fd00bf2fa913868958441e73ef88e9f
content-deleted <contentdeleted@gmail.com>
2019-01-26 16:47:47 -0700

    fix: changed camera control

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public bool moving = true;
     public bool shake = false;
     public float intensity = 1;
-    public float speed = 0.5f;
-
-    public float yLocation = 0;
-    // Apply to the player as well
-    
 
     private System.Random rand =  new System.Random();
-    void Update()
-    {
-        if(moving) yLocation += speed;
-    }
-
     void LateUpdate() {
-        transform.position = new Vector3(0,yLocation,transform.position.z);
+        transform.position = new Vector3(0,0,transform.position.z);
         if(shake) {
             transform.position += new Vector3(intensity*(float)rand.NextDouble(), intensity*(float)rand.NextDouble(), 0);
         }
---
+++ Assets/Scripts/MoveWithCam.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithCam : MonoBehaviour
+{
+    //void FixedUpdate() {if(CameraController.singleton.moving) transform.position += new Vector3(0,CameraController.singleton.speed,0);}
+}
