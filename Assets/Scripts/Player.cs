5538860279c3ba7dec3e211e4a4188647bfe6fed
content-deleted <contentdeleted@gmail.com>
2019-01-26 16:03:39 -0700

    merge: added kevin's scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+    float moveSpeed = 0.25f;
+
+    public int selfEsteem = 5;
+
+    //singleton
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+
+
+    void FixedUpdate()
+    {
+        float X=Input.GetAxis("Horizontal");
+        float Y = Input.GetAxis("Vertical");
+        //if(X>0) StraiffRight();
+        //if(X<0) StraiffLeft();
+        Vector3 axis = new Vector3(X, Y,0);
+        transform.localPosition += axis * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/SelfEstimeBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfEsteemBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Player.instance.selfEsteem;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {}
+    public void decrementSelfEsteemBar()
+    {
+        //transform.localScale=new Vector3()
+    }
+}
