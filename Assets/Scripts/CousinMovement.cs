8cf5453edc5c5f92746913e326d9af13aadbb204
Kevin12525 <40506166+Kevin12525@users.noreply.github.com>
2019-01-26 17:37:33 -0700

    Cousins

---
+++ Assets/Scripts/CousinMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CousinMovement : MonoBehaviour
+{
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
