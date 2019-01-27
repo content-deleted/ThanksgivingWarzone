fec3839aa4459918b066fdc9e880f9c236b9027d
content-deleted <contentdeleted@gmail.com>
2019-01-27 01:19:43 -0700

    feat: added title

---
+++ Assets/Scripts/LoadOnTitle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOnTitle : MonoBehaviour
+{
+    public TextMesh textMesh;
+    public string nextSceneName;
+	void Update () {
+        if (Input.GetButton("Fire1")) {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(nextSceneName);
+            textMesh.text = "LOADING...";
+        }
+    }
+}
